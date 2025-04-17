ec6374b04fc36d495e9af589e97dc0a9d849645b
vmesquita <vmesquita@311-P09>
2025-04-17 11:57:19 +0200

    Building

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Désactiver le clic droit et les raccourcis clavier
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+document.addEventListener('keydown', (e) => {
+    if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I")) {
+        e.preventDefault();
+    }
+});
+
+// Ajoute un message de bienvenue
 console.log("Bienvenue sur mon portfolio !");
